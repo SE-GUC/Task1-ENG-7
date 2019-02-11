f7ed20f7ee9a79bd8f7ce54fb1e33ed6e21927b6
Ahmed Khalifa <medoduh@gmail.com>
2019-02-12 00:36:09 +0200

    Add student entry to student array

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const students = [
+    { id: "40-5216", name: "Ahmed Mamdouh Khalifa", github_username: "AhmedKhalifaAhmed", email: "ahmed.khalifaahmed@student.guc.edu.eg" },
     { id: "28-09121", name: "Omar Sherif", github_username: "osheriff", email: "omarr@whatever.com" },
     { id: "21-094123", name: "Mathew White", github_username: "matheww", email: "matheww@whatever.com" },
     { id: "15-10312", name: "Dom Sundle", github_username: "domss", email: "domss.whatever.com" },
